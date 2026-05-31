bdb5da9438fbd8b5cbc4c95afbb6c1d620a161fe
mohammad <mohammad7979salehi@gmail.com>
2026-05-31 17:00:24 +0330

    modal & dropdown done

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { SearchableSelect } from "panta_design_system";
+import { Button, SearchableSelect, Modal } from "panta_design_system";
 
 import { useMemo, useState } from 'react';
 type Exchange = { id: number; name: string };
@@ -12,8 +12,15 @@ const EXCHANGES: Exchange[] = [
   { id: 5, name: "اقتصاد بیدار" },
 ];
 
+const userData = {
+  name: "علی رضایی",
+  email: "ali@example.com",
+  role: "مدیر محصول",
+  status: "فعال",
+};
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
 
   const [exchangeSelected, setExchangeSelected] = useState("");
 
@@ -23,20 +30,41 @@ export default function Home() {
   );
   return (
     <div style={{}}>
-    <div className="max-w-sm">
-      <SearchableSelect
-        label="کارگزاری"
-        value={exchangeSelected}
-        onChange={setExchangeSelected}
-        placeholder="همه کارگزاری‌ها"
-        allLabel="همه کارگزاری‌ها"
-        searchable
-        searchPlaceholder="جستجوی نام کارگزاری..."
-        options={options}
-        direction="rtl"
-        className="text-sm"
-      />
-    </div>
+      <div className="max-w-sm">
+        <SearchableSelect
+          label="کارگزاری"
+          value={exchangeSelected}
+          onChange={setExchangeSelected}
+          placeholder="همه کارگزاری‌ها"
+          allLabel="همه کارگزاری‌ها"
+          searchable
+          searchPlaceholder="جستجوی نام کارگزاری..."
+          options={options}
+          direction="rtl"
+          className="text-sm"
+        />
+
+      </div>
+      <button
+        type="button"
+        onClick={() => setOpen(true)}
+        className="lux-btn rounded-xl px-4 py-2"
+      >
+        Open Modal
+      </button>
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        title="تست مودال"
+        showCloseButton={false}
+      >
+        <p className="text-sm m-0 mb-4 pb-4">
+          این یک محتوای استاتیک برای تست کامپوننت Modal است.
+        </p>
+        <div className="mt-4">
+          <Button onClick={() => {setOpen(false)}} variant="danger" className="mt-4">بسته</Button>
+        </div>
+      </Modal>
     </div>
   );
 }
